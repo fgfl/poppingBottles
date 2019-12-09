29ecf54d9e591f1e0a32ad70faa2304d19e094b5
Frederick <frederick.gfl@gmail.com>
2019-12-09 08:04:59 +0000

    Add additional output to show stats
    - change return type to object that holds
      - total bottles
      - bottles from returning caps
      - botles from returning empty bottles

--- calcBottles.js
+++ calcBottles.js
@@ -7,6 +7,23 @@
 
   Given the above parameters, write a program so that you can figure out how many total
   bottles of pop can be redeemed given a customer investment.
+
+  Task 2
+  Have your program accept an argument (argv) that will allow the user to specify the amount
+  (in dollars) that the customer is going to spend. The node script will then calculate the
+  total number of bottles that the customer will receive.
+
+  Task 3
+  Enhance the output of your program so that once the amount has been given, it provides a breakdown
+  of how many bottles were purchased, how many were obtained through bottle recycling, and how many
+  were obtained through bottle cap recycling.
+
+  Sample:
+  Total Bottles: 75
+  Total Earned:
+    Bottles: 37
+    Caps: 18
+
 */
 
 // caculates the number of bottles you can get from an initial dollar investment
@@ -16,27 +33,50 @@ const calcBottles = (investment, caps = 0, emptyBottles = 0) => {
   const capsPerBottle = 4;
   const emptyPerFull = 2;
 
-  let totalBottles = 0;
+  let total = {
+    bottles: 0,
+    fromCaps: 0,
+    fromEmpties: 0,
+  }
 
   if (investment < dollarPerBottle && caps < capsPerBottle && emptyBottles < emptyPerFull) {
     // base case. can't get any more full bottles.
-    return 0;
+    return total;
   }
 
+  const fullBottlesFromCaps = Math.floor(caps / capsPerBottle);
+  const fullBottlesFromEmpties = Math.floor(emptyBottles / emptyPerFull);
   const fullBottles = 
     Math.floor(investment / dollarPerBottle) +
-    Math.floor(caps / capsPerBottle) + 
-    Math.floor(emptyBottles / emptyPerFull);
+    fullBottlesFromCaps +
+    fullBottlesFromEmpties;
   const remainingCaps = fullBottles + caps % capsPerBottle;
   const remainingEmptyBottles = fullBottles + emptyBottles % emptyPerFull;
 
-  totalBottles += fullBottles + calcBottles(investment % dollarPerBottle, remainingCaps, remainingEmptyBottles);
+  total.bottles += fullBottles;
+  total.fromCaps += fullBottlesFromCaps;
+  total.fromEmpties += fullBottlesFromEmpties;
 
-  return totalBottles;
+  const bottlesFromRecycling = calcBottles(
+    investment % dollarPerBottle,
+    remainingCaps,
+    remainingEmptyBottles);
+  for (key of Object.keys(bottlesFromRecycling)) {
+    total[key] += bottlesFromRecycling[key];
+  }
+
+  return total;
 };
 
 module.exports = calcBottles;
 
 const initialInvestment = ([...process.argv].slice(2) | 0);
+const totalBottles = calcBottles(initialInvestment);
 
-console.log(`With $${initialInvestment}, you will get ${calcBottles(initialInvestment)} bottles of pop.`);
+console.log(
+  `With $${initialInvestment}, you will get:
+  Total Bottles: ${totalBottles.bottles}
+  Total Earned Through:
+    Empty Bottles: ${totalBottles.fromEmpties}
+    Caps: ${totalBottles.fromCaps}`
+);
